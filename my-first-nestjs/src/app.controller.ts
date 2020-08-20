d117986529aea9afa49d0b8d345ba6d04a2cba27
somnath pal <somnath.pal1@comviva.com>
2020-08-20 10:15:38 +0530

    ui-um

---
+++ my-first-nestjs/src/app.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Response } from 'express';
+
+import * as BODY_Response from './component.json';
+import * as HEADER_Response from './header.json';
+import * as VIEW_Response from './view.json';
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get("/components")
+  getComponent(@Res() res: Response):any{
+    console.log("getting component .........." + Date.now())
+    res.status(HttpStatus.OK).json(BODY_Response);
+  }
+
+  @Get("/header")
+  getHeader(@Res() res: Response):any{
+    console.log("getting header .........." + Date.now())
+    res.status(HttpStatus.OK).json(HEADER_Response);
+  }
+
+  @Get("/view-json")
+  viewComponent(@Res() res: Response):any{
+    console.log("getting component .........." + Date.now())
+    res.status(HttpStatus.OK).json(VIEW_Response);
+  }
+}
