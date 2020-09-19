173c05022e3f3c16427c0181a0acceedbcf97df7
somnath pal <somnath.pal1@comviva.com>
2020-09-19 10:04:10 +0530

    some changes

--- my-first-nestjs/src/app.module.ts
+++ my-first-nestjs/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { AuthController } from './authentication/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
+  controllers: [AppController, UserController,AuthController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
